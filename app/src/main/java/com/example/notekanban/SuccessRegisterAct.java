db4ab1550885e3fada428e2324621e685832b8a8
nurzain-pradana <pradananurzain@gmail.com>
2020-02-23 17:43:33 +0700

    selesaikan xml

---
+++ app/src/main/java/com/example/notekanban/SuccessRegisterAct.java
@@ -0,0 +1,14 @@
+package com.example.notekanban;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuccessRegisterAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_register);
+    }
+}
